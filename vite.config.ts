acaddbebbd11755e0ab1186fc7df777993db796a
Grand Paradis <94236417+GRivals@users.noreply.github.com>
2025-12-05 14:49:24 +0000

    Open SD

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["f48ty5-8080.csb.app"], // разрешаем CSB
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
